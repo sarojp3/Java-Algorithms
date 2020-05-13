10540d0215de4dc75e56e4f1e6f9dd5e1f133f1b
Saroj Pandey <51286836+sarojp3@users.noreply.github.com>
2020-05-13 22:26:38 +0545

    Update SelectionSort.java
    Updated Commit

--- SelectionSort.java
+++ SelectionSort.java
@@ -4,6 +4,7 @@ public class SelectionSort {
 		int N = a.length;
 		for(int i=0;i<N;i++) {
 			int min = i;
+			//Iterating to find the minimum item from array on right side
 			for(int j=i+1;j<N;j++) {
 				if(less(a[j],a[min])) {
 					min = j;
