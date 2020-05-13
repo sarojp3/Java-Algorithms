ae45a40590b64ecc28225feb3c5c0ff05d397c68
Saroj Pandey <51286836+sarojp3@users.noreply.github.com>
2020-05-13 22:22:54 +0545

    Add files via upload
    Initial Commit

---
+++ Mergesort.java
@@ -0,0 +1,60 @@
+
+public class Mergesort {
+	
+	//Creating another array with same size as array a
+	public int[] sort(int[] a) {
+		int[] aux = new int[a.length];
+		sort(a,aux,0,a.length-1);
+		return a;
+	}
+
+	private int[] merge(int[] a, int[] aux, int lo, int mid, int hi) {
+		//Copy the array a to another array aux
+		for(int k=lo;k<=hi;k++) {
+			aux[k] = a[k];
+		}
+		//Merging
+		int i=lo , j=mid+1;
+		for(int k=lo;k<=hi;k++) {
+			if(i>mid) {
+				a[k] = aux[j];       //We can also write a[k] = aux[j++] to reduce the lines of code. Both are same.
+				j++;
+			}
+			else if(j>hi) {
+				a[k] = aux[i++];
+			}
+			else if(less(aux[j], aux[i])) {
+				a[k] = aux[j++];
+			}
+			else {
+				a[k] = aux[i++]; 
+			}
+		}
+		return a;
+	}
+	//Sorting two arrays recursively and merging them using merge function
+	public int[] sort(int[] a, int[] aux, int lo, int hi) {
+		if(hi<=lo) return a;
+		int mid = lo + (hi-lo)/2;
+		sort(a,aux,lo,mid);
+		sort(a, aux, mid+1,hi);
+		merge(a,aux,lo,mid,hi);
+		return a;
+	}
+	
+	private boolean less(int a, int b) {
+		return a<b;
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {1,5,9,2,6,3,4,8,7};
+		Mergesort ms = new Mergesort();
+		int[] sortedArray = ms.sort(array);
+		for(int i=0;i<array.length;i++) {
+			System.out.print(sortedArray[i]);
+			System.out.print(" ");
+		}
+	}
+	
+
+}
